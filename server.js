4940cf77680d303eab99510abb59358713092802
oliver fu <ofu997@gmail.com>
2018-07-16 15:40:39 -0700

    + project, ensuring that webpack and node are running

---
+++ server.js
@@ -0,0 +1,15 @@
+"use strict"
+var express = require("express");
+var app = express();
+var path= require('path');
+
+// Middleware to define static images
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
+
+app.listen(3000, function(){
+  console.log('app is running on localhost 3000');
+})
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+
+const webpack= require('webpack');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  // recompiles bundle.js on changes
+  watch: true,
+  module:{
+    // rules was previously loaders: [
+    rules: [
+      {
+        // loader, scan all js files
+        test:/\.js$/,
+        exclude:/node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
+      }
+    ]
+  }
+}
